5b4eb82c6fec94376ec0d56c39fd6f222608b673
muskan01-sahu <muskansahu069@gmail.com>
2026-05-10 21:25:09 +0530

    correction

--- Frontend/src/Register.jsx
+++ Frontend/src/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import { API } from "./Api";
+import { API } from "./api";
 import "./styles.css";
 
 const Register = () => {
